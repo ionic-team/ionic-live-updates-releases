75e843865e482ceeaa4f44db5ab805b073d59785
Steven Sherry <steven.r.sherry@gmail.com>
2024-02-06 16:31:28 -0600

    Update manifests for v0.5.0

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     targets: [
       .binaryTarget(
         name: "IonicLiveUpdates",
-        url: "https://github.com/ionic-team/ionic-live-updates-releases/releases/download/0.5.0-beta.1/IonicLiveUpdates.xcframework.zip",
-        checksum: "54e7ceeab70bbbf75ac66bef6fc62b8ee0cbf130a909790b294cbc94cf67cab7"
+        url: "https://github.com/ionic-team/ionic-live-updates-releases/releases/download/0.5.0/IonicLiveUpdates.xcframework.zip",
+        checksum: "0be64803ebf6c0ee20b7709e7f0646e1389f222ac5b2ae8a6dafeba391f6c064"
       )
     ]
 )
