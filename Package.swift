d41d1d700772ce8b3bcdac5f3dcfd3729f059057
Dan Giralte <dan.giralte@gmail.com>
2024-03-01 09:06:41 -0600

    builds for visionOS - note I needed the xcframework here to get the references right in other places.

--- Package.swift
+++ Package.swift
@@ -1,21 +1,22 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 import PackageDescription
 
 let package = Package(
     name: "IonicLiveUpdates",
-    platforms: [ .iOS(.v13) ],
+    platforms: [ .iOS(.v13), .visionOS(.v1) ],
     products: [
-      .library(
-        name: "IonicLiveUpdates", 
-        targets: ["IonicLiveUpdates"]
-      ),
+        .library(
+            name: "IonicLiveUpdates",
+            targets: ["IonicLiveUpdates"]
+        ),
     ],
     dependencies: [],
     targets: [
       .binaryTarget(
         name: "IonicLiveUpdates",
-        url: "https://github.com/ionic-team/ionic-live-updates-releases/releases/download/0.5.0/IonicLiveUpdates.xcframework.zip",
-        checksum: "0be64803ebf6c0ee20b7709e7f0646e1389f222ac5b2ae8a6dafeba391f6c064"
+        path: "IonicLiveUpdates.xcframework.zip"
+        //url: "https://github.com/ionic-team/ionic-live-updates-releases/releases/download/0.5.0/IonicLiveUpdates.xcframework.zip",
+        //checksum: "0be64803ebf6c0ee20b7709e7f0646e1389f222ac5b2ae8a6dafeba391f6c064"
       )
     ]
 )
